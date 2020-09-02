060372d01f5a5a5f6e4d30fbfe750dba7dd6561c
adrian <mendezadrian149@gmail.com>
2020-09-01 20:53:13 -0400

    changing ways to load graphql schema and resolvers

--- src/api/graphql/index.ts
+++ src/api/graphql/index.ts
@@ -1,15 +1,5 @@
 import path from 'path';
 import { mergeResolvers, mergeTypeDefs, loadFilesSync } from 'graphql-tools';
 
-let ext = 'js';
-
-if (process.env.NODE_ENV === 'test') {
-  ext = 'ts';
-}
-
-const types = loadFilesSync(path.join(__dirname, '/**/*.graphql'));
-const resolvePath = loadFilesSync(path.join(__dirname, `/resolvers/*.${ext}`));
-
-// Export graphql file and resolvers
-export const typeDefs = mergeTypeDefs(types);
-export const resolvers = mergeResolvers(resolvePath);
+export const typeDefs = mergeTypeDefs( loadFilesSync(path.join(__dirname, '/**/*.graphql')));
+export const resolvers = mergeResolvers(loadFilesSync(path.join(__dirname, `/resolvers/*.resolver.*`)));
