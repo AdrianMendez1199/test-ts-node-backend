a06dfdcb3eb7c9433430166fffe567b857087bfe
adrian <mendezadrian149@gmail.com>
2020-09-02 15:01:25 -0400

    working on create and assing ticket

--- src/api/graphql/resolvers/ticket.resolvers.ts
+++ src/api/graphql/resolvers/ticket.resolvers.ts
@@ -1,10 +1,11 @@
 import { Ticket } from '@entity/Ticket';
+import { User } from '@entity/User';
 
 export default {
 
     Mutation: {
         /**
-         * 
+         *  create and assing ticket
          * @param {void} _ 
          * @param {Object} args
          * @property {Object} data  
@@ -14,8 +15,21 @@ export default {
             args.data.user = ctx.request.auth.id;
             const ticket = Ticket.create(args.data);
             await ticket.save();
-            return ticket
+            return ticket;
         },
+    },
+
+
+    Ticket: {
+        /**
+         * find ticket create by user
+         * @param {Ticket} parent
+         * @property {int} id   
+         */
+        async createdBy(parent: Ticket): Promise<User | undefined> {
+            const ticket = await Ticket.findOne(parent.id, { relations: ['user'] });
+            return ticket?.user;
+        }
     }
 
 }
